9e8f8d1c918d9fdfb6a961d9db230167ba2c05a0
SOLARTE, ANDRES <ANDRES.SOLARTE@libertymutual.com>
2021-01-19 17:49:24 -0500

    feat: default handler.js, when the process uuid does not exists the connection must be adde by uuid and connectionId input params

--- backend/src/handlers/default.js
+++ backend/src/handlers/default.js
@@ -2,11 +2,13 @@ const { getByUuid, updateTimestamp } = require('../db')
 
 module.exports.handler = async (event, context, callback) => {
   const { uuid } = JSON.parse(event.body)
+  const connectionId = event.requestContext.connectionId
 
   if (uuid) {
     const uuidResult = await getByUuid(uuid)
 
     if (uuidResult) await updateTimestamp(uuid)
+    else await addConnection(uuid, connectionId)
   }
 
   callback(null, {
