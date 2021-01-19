49c7823477cc8729ff6871da7f21f582fd1c0aa8
SOLARTE, ANDRES <ANDRES.SOLARTE@libertymutual.com>
2021-01-19 15:25:21 -0500

    refactor: stream.js handler

--- backend/src/handlers/stream.js
+++ backend/src/handlers/stream.js
@@ -1,9 +1,15 @@
 module.exports.handler = async (event, context, callback) => {
   console.log(JSON.stringify(event, null, 2))
-  for (const record of event.Records) {
-    if (record.eventName === 'REMOVE') console.log('ES DE REMOVE !!!')
-    else console.log('ES DE OTRA COSA ...')
-  }
+
+  event
+    .Records
+    .filter(record => record.eventName === 'REMOVE')
+    .forEach(record => {
+      console.log(
+        'ES DE REMOVE, VA PARA SQS DE SALESFORCE !!!',
+        JSON.stringify(record, null, 2)
+      )
+    });
 
   callback(null, {
     statusCode: 200,
